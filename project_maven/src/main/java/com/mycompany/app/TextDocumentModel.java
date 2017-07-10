a69e40801c9a0491056aa1f011b8ea9ead483c81
saravidinha <saravidinha@Joaos-MacBook-Pro.local>
2017-07-11 00:15:21 +0100

    Update on TexDocumentModel
    Added line iteration of TextDocument content to discover the different
    types of TextLine, ClassDefinition, MethodsDefinition,
    VariableDefinition or UndefinedDefinition.

--- project_maven/src/main/java/com/mycompany/app/TextDocumentModel.java
+++ project_maven/src/main/java/com/mycompany/app/TextDocumentModel.java
@@ -1,48 +1,72 @@
 package com.mycompany.app;
 
+import java.awt.List;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.ArrayList;
 
 public class TextDocumentModel {
 	
 	// Definition of DocumentText line
-	public static class TextLine {
-		final int line;
-		final String documentText;
+	public static abstract class TextLine {
+		final int lineNumber;
+		final String lineText;
 		
-		
-		public TextLine(int line, String documentText) {
-			this.line = line;
-			this.documentText = documentText;
+
+		public TextLine(int lineNumber, String lineText) {
+			this.lineNumber = lineNumber;
+			this.lineText = lineText;
 		}
 
     }	
-
-
-
-	public static class Route extends TextLine {
-		final String name;
-		public Route(int line, String documentText, String name) {
-			super(line, documentText);
-			this.name = name;
+	
+	public static class ClassDefinition extends TextLine{
+		final String className;
+		
+		public ClassDefinition (int lineNumber,String lineText,String className){
+			super(lineNumber,lineText);
+			this.className=className;
 		}
+		
+
 	}
+	
+	public static class MethodsDefinition extends TextLine{
+		final String methodsName;
+		
+		public MethodsDefinition(int lineNumber, String lineText,String methodsName){
+			super(lineNumber,lineText);
+			this.methodsName=methodsName;
+		}
+		
+		
+	}
+	
 	public static class VariableDefinition extends TextLine {
 		final String variableName;
 		final String variableValue;
 		
-		public VariableDefinition(int lineNumber,String documentText, String variableName, String value) {
-			super(lineNumber,documentText);
+		public VariableDefinition(int lineNumber,String lineText, String variableName, String value) {
+			super(lineNumber, lineText);
 			this.variableName = variableName;
 			variableValue = value;
 		}
-
+	}
+	//to store info of the code that do not belong to ClassDefinition, MethodsDefinition or VariableDefinition
+	public static class UndefinedContent extends TextLine{
+		public UndefinedContent(int lineNumber, String lineText){
+			super(lineNumber, lineText);
+		}
 		
 	}
 
+private final ArrayList<TextLine> lines = new ArrayList<>();
+
 // Separation of the DocumentText in lines 
-public TextDocumentModel(String documentText){
+//Contructor can trow IOExceotuib on usage of Reader and BufferReader.
+public TextDocumentModel(String documentText) throws IOException{
 
 	try (
 			Reader reader= new StringReader(documentText);
@@ -50,10 +74,53 @@ public TextDocumentModel(String documentText){
 		) {
 			String lineText;
 			int lineNumber = 0;
+			
 			while ((lineText = buffereReader.readLine()) != null) {
+				if (lineText.contains("class")){
+					String[] content = lineText.split(" ");
+					int index;
+					for(index=0;index<content.length;index ++){
+						if (content[index].equals("class"))
+							break;	
+					}
 				
+					String className=content[index+1];
+				    ClassDefinition classDefinition = new ClassDefinition (lineNumber, lineText, className);
+					lines.add(classDefinition);
+				}
+				else {
+					if(lineText.contains("(") && lineText.contains(")") && lineText.contains("{")){
+						 int index = lineText.indexOf("(");
+						 String splited = lineText.substring(0, index);
+						 String[] content = splited.split(" ");
+						 
+						 String methodsName = content[content.length-1];
+						 
+						 MethodsDefinition methodsDefinition = new MethodsDefinition(lineNumber, lineText, methodsName);
+						 lines.add(methodsDefinition);
+					}
+					else{
+						if(lineText.contains("=")){
+							String[] content = lineText.split("=");
+							
+							String name = content[0];
+							String value = content[1];
+							
+							VariableDefinition variableDefinitio = new VariableDefinition(lineNumber, lineText, name, value);
+							lines.add(variableDefinitio);
+						}
+						else{
+							UndefinedContent undefinedContent = new UndefinedContent(lineNumber, lineText);
+							lines.add(undefinedContent);
+						}
+							
+					}
+					
+				}
 			}
+				
 	}
+}
 		
 
 
